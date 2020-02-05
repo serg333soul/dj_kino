302a696c798432ae1517ffca139ce24e8aea3c13
Sergey <sergey@mail.com>
2020-02-05 22:28:21 +0200

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField('Имя', max_length=100)
+    slug = models.SlugField('url', max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+from . models import Category
+# Create your views here.
+
+class HomeView(View):
+
+    def get(self, request):
+        context = Category.objects.all()
+        print(context)
+        return render(request, 'blog/home.html', {'context': context})
+        
