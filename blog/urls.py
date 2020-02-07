1cf206742b3e83a0473647186a6a830e9c454e64
Sergey <serg333soul@gmail.com>
2020-02-07 16:28:26 +0200

    end lesson 2

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<slug:slug>/', views.CategoryView.as_view()),
+    path('<slug:slug>/', views.CategoryView.as_view(), name='category'),
     path('', views.HomeView.as_view()),
 ]
