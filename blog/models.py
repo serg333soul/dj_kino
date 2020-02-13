495f409364741ca3fe82f2449de93be3467b6cbc
Sergey <sergey@mail.com>
2020-02-13 22:16:20 +0200

    hw less3 blog_models_category

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from datetime import datetime
+from mptt.fields import TreeForeignKey
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField('Имя', max_length=100)
     slug = models.SlugField('url', max_length=100)
+    description = models.TextField('Описание', max_length=1000, default='', blank=True)
+    parent = TreeForeignKey(
+        'self',
+        verbose_name = 'Родительская категория',
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True,
+        related_name = 'children'
+    )
+    template = models.Charfield('Шаблон', max_length=500, default='blog/post_list.html')
+    published = models.BooleanField('Отображать?', default=True)
+    paginated = models.PositiveIntegerField('Количество новостей на странице', default=5)
+    sort = models.PositiveIntegerField('Порядок', default=0)
 
     def __str__(self):
         return self.name
