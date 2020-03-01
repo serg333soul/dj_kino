57bfa53185f640548c833178834900987f8063d4
Sergey <sergey@mail.com>
2020-03-01 21:31:33 +0200

    create PostListView

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("<slug:category_slug>/", views.PostListView.as_view(), name='category'),
+
     path('tag/<slug:slug>/', views.TagDetailView.as_view(), name='tag'),
     path('<slug:category>/<slug:slug>/', views.PostDetailView.as_view(), name='detail_post'),
     path('<slug:category_name>/', views.CategoryView.as_view(), name='category'),
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,27 @@ from datetime import datetime
 from django.views.generic.base import View
 from .models import Category, Tag, Post, Comment
 
+class PostListView(View):
+    '''Вывод статьей категории'''
+    def get_queryset(self):
+        return Post.objects.filter(published_date_lte=datetime.now(), published=True)
+    
+    def get(self, request, category_slug=None, slug=None):
+        category_list = Category.objects.filter(published=True)
+        if category_slug is not None:
+             posts = self.get_queryset().filter(category__slug=category_slug,
+                                                category__published=True
+                                                )
+        elif slug is not None:
+            posts = self.get_queryset().filter(tags__slug=slug, tags__published=True)
+        else:
+            posts = self.get_queryset()
+        if posts.exists():
+            template = 'blog/post_list.html'
+        return render(request, template, {
+            'post_list': posts,
+            'catigories': category_list
+        })    
 
 class HomeView(View):
     '''Home page'''
@@ -10,7 +31,7 @@ class HomeView(View):
         category_list = Category.objects.all()
         post_list = Post.objects.filter(published_date__lte=datetime.now(), published=True)
         return render(request, 'blog/post_list.html', {'categories': category_list, 'post_list': post_list})
-
+        
 class TagDetailView(View):
     def get(self, request, slug):
         tag_list = Post.objects.filter(tags__slug=slug, tags__published=True)
@@ -41,4 +62,4 @@ class CategoryView(View):
         posts = Post.objects.filter(category_id=category.id)
         return render(request, 'blog/category_detail.html', {'category': category, 'posts': posts})
 
-           
+
